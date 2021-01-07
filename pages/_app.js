3b43c2ddc2bf50d424e2c281c5ff9ca0cae205ee
DevDuki <dusan-misic@hotmail.com>
2021-01-07 23:05:34 +0100

    added style to the sites with styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,30 @@
-import Link from 'next/link'
+import styled from 'styled-components'
+import { Normalize } from 'styled-normalize'
+import Navbar from '../components/Navbar'
+
+const Container = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Padauk:wght@400;700&display=swap');
+  min-height: 100vh;
+  background: linear-gradient(to right, #43c6ac, #191654);
+  font-family: 'Padauk', sans-serif;
+  color: #444;
+`
+
+const Page = styled.div`
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+`
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Component {...pageProps} />
-    </>
+    <Container>
+      <Normalize />
+      <Navbar />
+      <Page>  
+        <Component {...pageProps} />
+      </Page>
+    </Container>
   )
 }
 
--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,16 +1,46 @@
 import fs from 'fs'
 import matter from 'gray-matter'
 import marked from 'marked'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background: white;
+  padding: 1rem 2rem;
+  margin: 1rem 0;
+`
+
+const Title = styled.div`
+  display: flex;
+  align-items: flex-end;
+`
+
+const Subtitle = styled.p`
+  padding: 0.75rem 0.5rem;
+  color: #444;
+`
+
+const Price = styled.span`
+  font-size: 2rem;
+  background: #42c4ab;
+  padding: 0.25rem 1rem;
+  border-radius: 5px;
+  color: white;
+  font-weight: 500;
+  margin-bottom: 1rem;
+  display: inline-block;
+`
 
 const Product = ({ product: { data, content } }) => {
   const html = marked(content) // converts the markdown syntax into an html syntax
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <p>{data.price/100}€</p>
+    <Container>
+      <Title>
+        <h1>{data.name}</h1>
+        <Subtitle>{data.description}</Subtitle>
+      </Title>      
+      <Price>{data.price/100}€</Price>
       <div dangerouslySetInnerHTML={{ __html: html }} />
-    </div>
+    </Container>
   )
 }
 
@@ -22,7 +52,7 @@ export const getStaticPaths = () => {
   const paths = filenames.map((filename) => {
     return {
       params: {
-        product: filename.replace('.md', '')
+        product: filename.replace('.md', '') // Name of this attribute here, has to match the name of the file in brackets [], in our case [product].js
       }
     }
   })
